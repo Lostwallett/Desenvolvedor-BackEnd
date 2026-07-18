8d6bdeecc3863dfefe871bb7019ab55441ba6304
Lostwallett <rsilva.mj@gmail.com>
2026-07-17 21:51:13 -0300

    Finalizado aula 1 dia 17/07/26.

---
+++ Lógica de Programação e Algoritmos/Aula01/SaidaDeDados/SaidaDeDados/Program.cs
@@ -0,0 +1,19 @@
+﻿using static System.Console;
+
+Console.WriteLine("Hello, World!");
+WriteLine("This is a test of the C# 14.0 features.");
+
+int numero = 12;
+double flutuante = 3.14;
+string texto = "Olá, mundo!";
+char caracter = 'A';
+bool booleano = true;
+bool booleano2 = false;
+WriteLine(numero);
+WriteLine($"O número é: {numero}");
+
+
+
+
+
+ReadKey();
