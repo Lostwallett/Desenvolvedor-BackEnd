8328730fe6ebdee9f3b9d1b5f74c6c5a3a575bf1
Lostwallett <rsilva.mj@gmail.com>
2026-07-20 21:53:23 -0300

    Entrada,Declaração de Variáveis, Processamento e Saída de Dados

---
+++ Lógica de Programação e Algoritmos/Aula02/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using static System.Console;
+
+// Declaração de variáveis
+
+//double largura, comprimento, area, valor, preco; ---> pode ser feito assim.
+
+double largura;
+double comprimento;
+double area;
+double valor;
+double preco;
+
+Write("Digite a largura do terreno: ");
+largura = double.Parse(Console.ReadLine());
+
+Write("Digite o comprimento do terreno: ");
+comprimento = double.Parse(Console.ReadLine());
+
+Write("Digite o valor do metro quadrado: ");
+valor = double.Parse(Console.ReadLine());
+
+// Processamento de dados
+
+area = largura * comprimento;
+preco = area * valor;
+
+// Saída de dados
+
+WriteLine($"A área do terreno é: {area:n} m²\nO preço do terreno: R$ {preco:n}");
---
+++ Lógica de Programação e Algoritmos/Aula02/ConsoleApp1/ExercicioRetangulo/Program.cs
@@ -0,0 +1,27 @@
+﻿using static System.Console;
+
+// Declaração de variáveis
+
+double base_ret;
+double altura;
+double area;
+double perimetro;
+double diagonal;
+
+Write("Digite a base do retângulo: ");
+base_ret = double.Parse(Console.ReadLine());
+
+Write("Digite a altura do retângulo: ");
+altura = double.Parse(Console.ReadLine());
+
+// Processamento de dados
+
+area = base_ret * altura;
+perimetro = (altura + base_ret) * 2;
+diagonal = Math.Sqrt(Math.Pow(base_ret, 2) + Math.Pow(altura ,2));
+//diagonal = Math.Sqrt((base_ret * base_ret) + (altura * altura));
+
+// Saída de dados
+
+//WriteLine($"A área do retângulo é: {area.ToString("F4")} m²\nO perímetro do retângulo é: {perimetro.ToString("F4")} m\nA diagonal do retângulo é: {diagonal.ToString("F4")} m"); ---> 4 decimais usando ToString("F4")
+WriteLine($"A área do retângulo é: {area:F4} m²\nO perímetro do retângulo é: {perimetro:F4} m\nA diagonal do retângulo é: {diagonal:F4} m");
