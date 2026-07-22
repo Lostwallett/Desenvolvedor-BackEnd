fa2f1601a1f24284727ec9e8351e62b683997679
Lostwallett <rsilva.mj@gmail.com>
2026-07-22 20:10:53 -0300

    Atividade 01 - Estrutura sequencial de algoritmos

---
+++ Lógica de Programação e Algoritmos/Aula03/Operadores/Atividade 01 - Estrutura sequencial de algoritmos/Program.cs
@@ -0,0 +1,45 @@
+﻿using static System.Console;
+
+int primeiro;
+int segundo;
+int terceiro;
+
+Write("Digite um número: ");
+primeiro = int.Parse(Console.ReadLine());
+
+Write("Digite outro número: ");
+segundo = int.Parse(Console.ReadLine());
+
+Write("Digite mais um número: ");
+terceiro = int.Parse(Console.ReadLine());
+WriteLine();
+
+
+
+
+bool p1 = primeiro == terceiro;
+bool p2 = primeiro != segundo;
+bool p3 = segundo >= primeiro;
+bool p4 = segundo <= terceiro;
+
+
+
+
+WriteLine($"1. O primeiro valor é igual ao terceiro valor ? R: {(p1 ? "Verdadeiro" : "Falso")}");
+WriteLine($"2. O primeiro valor é diferente do segundo valor? R: {(p2 ? "Verdadeiro" : "Falso")}");
+WriteLine($"3. O segundo valor é maior que primeiro valor ? R: {(p3 ? "Verdadeiro" : "Falso")}");
+WriteLine($"4. O segundo valor é menor ou igual que terceiro valor? R: {(p4 ? "Verdadeiro" : "Falso")}");
+WriteLine();
+
+
+
+bool p5 = p1 == p3;
+bool p6 = p2 | p4;
+bool p7 = !p1;
+
+
+WriteLine($"1.Se a pergunta 1 e pergunta 3 são verdadeiros: {(p5 ? "Verdadeiro" : "Falso")}");
+WriteLine($"2.Se a pergunta 2 e pergunta 4 pelo menos uma é verdadeiro: {(p6 ? "Verdadeiro" : "Falso")}");
+WriteLine($"3.Se a negação da primeira pergunta é verdadeira: {(p7 ? "Verdadeiro" : "Falso")}");
+
+ReadKey();
---
+++ Lógica de Programação e Algoritmos/Aula03/Operadores/Operadores/Program.cs
@@ -0,0 +1,71 @@
+﻿using System.Runtime.Intrinsics.X86;
+using static System.Console;
+
+// Operadores recionais
+/*
+    
+    1. Igual a (==): Verifica se dois valores são iguais.
+       Exemplo: a == b
+    2. Diferente de (!=): Verifica se dois valores são diferentes.
+       Exemplo: a != b
+    3. Maior que (>): Verifica se o valor à esquerda é maior que o valor à direita.
+       Exemplo: a > b
+    4. Menor que (<): Verifica se o valor à esquerda é menor que o valor à direita.
+       Exemplo: a < b
+    5. Maior ou igual a (>=): Verifica se o valor à esquerda é maior ou igual ao valor à direita.
+       Exemplo: a >= b
+    6. Menor ou igual a (<=): Verifica se o valor à esquerda é menor ou igual ao valor à direita.
+       Exemplo: a <= b
+*/
+
+//int x = 1;
+//int y = 2;
+
+//double x = 1;
+//double y = 2;
+
+//WriteLine(x < y);
+//WriteLine(x <= y);
+//WriteLine(x > y);
+//WriteLine(x >= y);
+//WriteLine(x == y);
+//WriteLine(x != y);
+//WriteLine();
+
+//string a = "SENAI";
+//string b = "Americana";
+
+//WriteLine(a == b);
+//WriteLine(a != b);
+//WriteLine();
+
+//WriteLine(a == b ? "Verdadeiro" : "Falso");
+
+// Operadores lógicos
+/*
+ * ! - Negação: Inverte o valor lógico de uma expressão.
+ * & - E (AND): Retorna verdadeiro se ambas as expressões forem verdadeiras.
+ * | - OU (OR): Retorna verdadeiro se pelo menos uma das expressões for verdadeira.
+ * ^ - OU Exclusivo (XOR): Retorna verdadeiro se apenas uma das expressões for verdadeira.
+ * && - E Condicional (AND): Retorna verdadeiro se ambas as expressões forem verdadeiras, mas não avalia a segunda expressão se a primeira for falsa.
+ * || - OU Condicional (OR): Retorna verdadeiro se pelo menos uma das expressões for verdadeira, mas não avalia a segunda expressão se a primeira for verdadeira.
+ */
+
+
+bool x = true;
+bool y = false;
+
+WriteLine(!x);
+WriteLine(x & y);
+WriteLine(x | y);
+WriteLine(x ^ y);
+WriteLine(x && y);
+WriteLine(x || y);
+WriteLine();
+
+
+
+
+
+
+ReadKey();
