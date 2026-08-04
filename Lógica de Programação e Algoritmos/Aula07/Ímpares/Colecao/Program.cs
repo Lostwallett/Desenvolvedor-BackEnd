ff580a1d546dce8fd96bf8ee431cffd5e6fe3cdc
Lostwallett <rsilva.mj@gmail.com>
2026-08-03 21:55:38 -0300

    Começamos coleções de dados.

---
+++ Lógica de Programação e Algoritmos/Aula07/Ímpares/Colecao/Program.cs
@@ -0,0 +1,27 @@
+﻿using static System.Console;
+
+string[] nomes =
+{
+    "Maria", "João", "José", "Ana", "Pedro", "Paula", "Lucas", "Mariana", "Gabriel", "Beatriz"
+};
+
+
+WriteLine(nomes[0]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ReadKey();
