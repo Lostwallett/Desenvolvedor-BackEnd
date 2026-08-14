139e96358a334d9c0d4566f703c21a94b2dbd132
Lostwallett <rsilva.mj@gmail.com>
2026-08-14 19:48:41 -0300

    Versão 2.

--- Programação Orientada a Objetos/Aula03/Sexta/Aplicativo01/Program.cs
+++ Programação Orientada a Objetos/Aula03/Sexta/Aplicativo01/Program.cs
@@ -1,21 +1,23 @@
-﻿using static System.Console;
+﻿using Aplicativo01;
+using static System.Console;
 
 //Declaração de variáveis
-double raio, circunferencia, volume;
-const double pi = 3.1415;
+Calculadora calculadora;
+double raio;
 
 //Entrada de dados
 Write("Entre com o valor do raio da circunferência: ");
 raio = double.Parse(ReadLine()!);
 
+//Instanciação do objeto
+calculadora = new Calculadora(raio);
+
 //Processamento de dados
-circunferencia = 2 * pi * raio;
-volume = (4 * pi * Math.Pow(raio, 3)) / 3;
 
 //Saída de dados
-WriteLine($"Circunferência: {circunferencia:f2}\n" +
-    $"Volume: {volume:f2}\n" +
-    $"PI = {pi:f2}");
+WriteLine($"Circunferência: {calculadora.Circunferencia():f2}\n" +
+    $"Volume: {calculadora.Volume():f2}");
+calculadora.PI();
 
 
 
