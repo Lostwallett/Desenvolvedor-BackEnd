43113bc307c677dd65cd437eda19070416334e5b
Lostwallett <rsilva.mj@gmail.com>
2026-08-14 21:10:49 -0300

    Exercício Cotação do Dólar.

---
+++ Programação Orientada a Objetos/Aula03/Sexta/Moeda/ConversorDeMoedas.cs
@@ -0,0 +1,15 @@
+﻿namespace ConversorDeMoedas
+{
+    internal static class Cotacao
+    {
+        //Campos
+        public const double iof = 0.06;
+
+
+        public static double Conversor(double dolar, double comprar)
+        {
+            return dolar * comprar * (1 + iof);            
+        }
+        
+    }
+}
---
+++ Programação Orientada a Objetos/Aula03/Sexta/Moeda/Program.cs
@@ -0,0 +1,16 @@
+﻿using static System.Console;
+using ConversorDeMoedas;
+
+//Declaração de variáveis
+double dolar, comprar;
+
+Write("Qual é a cotação do dólar? ");
+dolar = double.Parse(ReadLine()!);
+
+Write("Quantos dólares você vai comprar? ");
+comprar = double.Parse(ReadLine()!);
+
+WriteLine($"Valor a ser pago em reais = {Cotacao.Conversor(dolar, comprar):c}");
+
+
+ReadKey();
