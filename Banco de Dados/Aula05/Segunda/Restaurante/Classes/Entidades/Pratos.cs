3c4f6ff6fd327044de8b9239934fc0211319471a
Lostwallett <rsilva.mj@gmail.com>
2026-09-14 21:53:36 -0300

    Realizado configurações no banco de dados para imagens.

---
+++ Banco de Dados/Aula05/Segunda/Restaurante/Classes/Entidades/Pratos.cs
@@ -0,0 +1,19 @@
+﻿namespace Restaurante.Classes.Entidades
+{
+    internal class Pratos
+    {
+        //Propriedades
+        public int Id { get; set; }
+        public string Nome_do_prato { get; set; }
+        public decimal Preco { get; set; }
+        public string Foto_do_prato { get; set; }
+    
+    //Construtores
+        public Pratos(string nome_do_prato, decimal preco, string foto_do_prato)
+        {
+            Nome_do_prato = nome_do_prato;
+            Preco = preco;
+            Foto_do_prato = foto_do_prato;
+        }
+    }
+}
